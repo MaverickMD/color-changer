86eda1c96ee68a6129f34227aa3deb1e573c7e9c
Jazmin <jazminsignups@gmail.com>
2025-11-02 15:30:47 -0500

    rgb text value and bg update successfully.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const eRGB_DISPLAY = document.querySelector('#rgbDisplay');
+const eCOLOR_CHANGE_BTN = document.querySelector('#colorChangeBtn');
+
+
 function randomNumber(){
   return Math.floor(Math.random()*255)+1
 }
@@ -9,3 +13,15 @@ function rgbValue(){
   return `rgb(${r},${g},${b})`
 }
 
+function changeBg(){
+  let currentColor = rgbValue();
+  document.querySelector('.main-wrapper').style.backgroundColor = currentColor
+  return currentColor
+}
+
+function updateDisplay(value){
+  eRGB_DISPLAY.innerHTML = value;
+}
+eCOLOR_CHANGE_BTN.addEventListener('click', ()=>{
+  updateDisplay(changeBg())
+})
